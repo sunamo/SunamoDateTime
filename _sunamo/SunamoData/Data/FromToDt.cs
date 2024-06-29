613d98075979132c5a2265248867aa0e1e4be2cf
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:37:47 +0200

    Make some classes public when it is needed

---
+++ _sunamo/SunamoData/Data/FromToDt.cs
@@ -0,0 +1,35 @@
+namespace SunamoDateTime;
+
+
+/// <summary>
+///     Must have always entered both from and to
+///     None of event could have unlimited time!
+/// </summary>
+internal class FromToDt : FromToTSHDt<long>
+{
+    internal static FromToDt Empty = new(true);
+    internal FromToDt()
+    {
+    }
+    /// <summary>
+    ///     Use Empty contstant outside of class
+    /// </summary>
+    /// <param name="empty"></param>
+    private FromToDt(bool empty)
+    {
+        this.empty = empty;
+    }
+    /// <summary>
+    ///     A3 true = DateTime
+    ///     A3 False = None
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="ftUse"></param>
+    internal FromToDt(long from, long to, FromToUse ftUse = FromToUse.DateTime)
+    {
+        this.from = from;
+        this.to = to;
+        this.ftUse = ftUse;
+    }
+}
