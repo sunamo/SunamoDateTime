9fc931113d3a468af2f553413258b75e32e769c4
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 08:57:39 +0200

    moved all in _sunamo to right subfolders by original project

---
+++ LangsDt.cs
@@ -0,0 +1,19 @@
+namespace SunamoDateTime;
+
+
+// Musí být ve SunamoEnums protože je sdílen např. i ve SunamoDateTime
+/// <summary>
+/// For projects for which is reference whole Xlf useless
+/// But it is only one file (like here Langs), consider import it instead create standalone project
+/// Tool which will copy it automatilly also could not be bad
+/// 
+/// Langs -> LangsDt protože hodně metod jej zde potřebuje a tím pádem musí být public
+/// 
+/// </summary>
+public enum LangsDt
+{
+    #region For easy copying to other files
+    cs = 0,
+    en = 1
+    #endregion
+}
---
+++ _sunamo/SunamoBts/BTS.cs
@@ -0,0 +1,44 @@
+namespace SunamoDateTime;
+
+// Bud 
+
+
+//namespace SunamoDateTime;
+internal static class BTS
+{
+    //    internal static Func<string, int?, int?> ParseInt;
+    //    internal static Func<bool, bool, string> BoolToStringEn;
+    //    internal static Func<bool, string> BoolToString;
+
+    internal static int? ParseInt(string entry, int? _default)
+    {
+        int lastInt2 = 0;
+        if (int.TryParse(entry, out lastInt2))
+        {
+            return lastInt2;
+        }
+        return _default;
+    }
+
+    internal static int TryParseInt(string entry, int def)
+    {
+        return TryParseInt(entry, def, false);
+    }
+
+    internal static int TryParseInt(string entry, int def, bool throwEx)
+    {
+        int lastInt = 0;
+        if (int.TryParse(entry, out lastInt))
+        {
+            return lastInt;
+        }
+        else
+        {
+            if (throwEx)
+            {
+                ThrowEx.NotInt(entry, null);
+            }
+        }
+        return def;
+    }
+}
